b3d8a9c76d8b2a70401f0f52099a1c26f85fcb85
Votre Nom <votre-email@example.com>
2026-02-28 00:11:35 +0000

    Updates

--- sw.js
+++ sw.js
@@ -85,7 +85,7 @@ define(['./workbox-15ec2ecc'], (function (workbox) { 'use strict';
     "revision": "9e820a797a6c7f5cd34a106890bd0752"
   }, {
     "url": "index.html",
-    "revision": "b02aca68223b7ef50a8242c59c008005"
+    "revision": "75b8d6c1823917e70c8f5e8a89da2c03"
   }, {
     "url": "404.html",
     "revision": "d0e9f9bdb9145562f5d9f35dca8b2840"
@@ -189,7 +189,7 @@ define(['./workbox-15ec2ecc'], (function (workbox) { 'use strict';
     "url": "assets/vendor-firebase-LntcAirY.js",
     "revision": null
   }, {
-    "url": "assets/index-DkrANkoy.js",
+    "url": "assets/index-ZFPvbV_v.js",
     "revision": null
   }, {
     "url": "assets/index-BRVgAE6r.css",
