b009cb7d6fbd58482499434b79bd825d9e142801
Votre Nom <votre-email@example.com>
2026-03-01 08:58:59 +0000

    Updates

--- sw.js
+++ sw.js
@@ -85,7 +85,7 @@ define(['./workbox-79eeca62'], (function (workbox) { 'use strict';
     "revision": "9e820a797a6c7f5cd34a106890bd0752"
   }, {
     "url": "index.html",
-    "revision": "f52387f82450bbf3a9e741213efbcd10"
+    "revision": "130c4845fb568ca1a391884c40162c4b"
   }, {
     "url": "404.html",
     "revision": "d0e9f9bdb9145562f5d9f35dca8b2840"
@@ -192,7 +192,7 @@ define(['./workbox-79eeca62'], (function (workbox) { 'use strict';
     "url": "assets/index-DyLbqzUy.css",
     "revision": null
   }, {
-    "url": "assets/index-DMGC18Qm.js",
+    "url": "assets/index-8MQpHWUr.js",
     "revision": null
   }, {
     "url": "manifest.json",
