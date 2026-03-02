3d08f294354ecc3904e1134a5c391d97d66d3603
Votre Nom <votre-email@example.com>
2026-03-02 12:02:31 +0000

    Updates

--- sw.js
+++ sw.js
@@ -85,7 +85,7 @@ define(['./workbox-79eeca62'], (function (workbox) { 'use strict';
     "revision": "9e820a797a6c7f5cd34a106890bd0752"
   }, {
     "url": "index.html",
-    "revision": "628de10b0488572096598050bc7ae3fc"
+    "revision": "cddc46c431b4505e0d6a043173fd94ec"
   }, {
     "url": "404.html",
     "revision": "d0e9f9bdb9145562f5d9f35dca8b2840"
@@ -189,10 +189,10 @@ define(['./workbox-79eeca62'], (function (workbox) { 'use strict';
     "url": "assets/vendor-firebase-LntcAirY.js",
     "revision": null
   }, {
-    "url": "assets/index-DBbaR_6-.js",
+    "url": "assets/index-nj10I2nL.js",
     "revision": null
   }, {
-    "url": "assets/index-Cvr4A5eU.css",
+    "url": "assets/index-CZdMlT2o.css",
     "revision": null
   }, {
     "url": "manifest.json",
