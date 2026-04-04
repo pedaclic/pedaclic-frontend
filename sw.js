1a0fcbc237458674c9b1cf8f968b665e367d922e
Votre Nom <votre-email@example.com>
2026-04-04 10:53:58 +0000

    Updates

--- sw.js
+++ sw.js
@@ -85,7 +85,7 @@ define(['./workbox-79eeca62'], (function (workbox) { 'use strict';
     "revision": "9e820a797a6c7f5cd34a106890bd0752"
   }, {
     "url": "index.html",
-    "revision": "a88e277774f04fd334eccb54fe2ffae5"
+    "revision": "0d444bda16e0184c2f8f02dac8cc9aca"
   }, {
     "url": "404.html",
     "revision": "6a70d336d00aecab451233c19c009113"
@@ -204,16 +204,16 @@ define(['./workbox-79eeca62'], (function (workbox) { 'use strict';
     "url": "assets/jspdf.plugin.autotable-CBbcTuFj.js",
     "revision": null
   }, {
-    "url": "assets/jspdf.es.min-DXFT2qDn.js",
+    "url": "assets/jspdf.es.min-7HbP9Vdu.js",
     "revision": null
   }, {
-    "url": "assets/index.es-CYUFQf7U.js",
+    "url": "assets/index.es-CoMwgnGR.js",
+    "revision": null
+  }, {
+    "url": "assets/index-DitzVmI4.js",
     "revision": null
   }, {
     "url": "assets/index-DYOuOtiW.css",
-    "revision": null
-  }, {
-    "url": "assets/index-BF1Nsge1.js",
     "revision": null
   }, {
     "url": "assets/html2canvas.esm-DXEQVQnt.js",
